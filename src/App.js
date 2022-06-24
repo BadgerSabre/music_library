dc3e967cb3acee677e6cb4a6c7f291f2fbccb014
Brandon Dega <brandondega@gmail.com>
2022-06-24 10:20:53 -0500

    added suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import SearchBar from './Components/SearchBar';
 import Gallery from './Components/Gallery'
+import { createResource as fetchData } from './helper'
+
+
 
 function App() {
   const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for Music!')
-  const [data, setData] = useState([])
+  const [data, setData] = useState(null)
+  
+  const renderGallery = () => {
+    if(data){
+        return (
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Gallery data={data} />
+            </Suspense>
+        )
+    }
+  }
 
   const handleSearch = (e, term) => {
     e.preventDefault()
@@ -13,27 +26,33 @@ function App() {
   }
 
   useEffect(() => {
-    if(search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(`https://itunes.apple.com/search?term=${search}`)
-        const data = await response.json()
-        console.log(data)
-        if(data.results.length > 0) {
-          setData(data.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
+    if (search) {
+        setData(fetchData(search))
     }
-  }, [search])
+}, [search])
+
+  // useEffect(() => {
+  //   if(search) {
+  //     const fetchData = async () => {
+  //       document.title = `${search} Music`
+  //       const response = await fetch(`https://itunes.apple.com/search?term=${search}`)
+  //       const data = await response.json()
+  //       console.log(data)
+  //       if(data.results.length > 0) {
+  //         setData(data.results)
+  //       } else {
+  //         setMessage('Not Found')
+  //       }
+  //     }
+  //     fetchData()
+  //   }
+  // }, [search])
 
   return (
     <div>
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data}/>
+      {renderGallery()}
     </div>
   );
 }
